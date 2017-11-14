b56ff9d921c4fe511169fdc12ec5fd9792bf3c8a
Chris Freeman <cfreeman29@gmail.com>
2017-11-13 23:30:25 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,61 @@
+# Exsilium Bot
+
+A Discord bot that assits members on the Exsilium Server
+
+## Add the live version to your server
+
+```
+TBD
+```
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+```
+Node.js
+Discord Account
+```
+
+### Installing
+
+Place the Source files in a dedicated directory.
+
+Install Node.js
+```
+https://nodejs.org/en/
+```
+Run the following command after installation
+
+```
+npm install --save discord.js
+```
+
+Then run the following from the source files directory
+```
+node bot.js
+```
+
+## Running the tests
+
+Look into how to get an API key on Discord and you need to put that code in bot.js on the last line
+```
+client.login('YOUR API HERE');
+```
+
+### Break down into end to end tests
+
+Follow the instructions on Discord for how to add your bot to your discord server
+
+```
+When you do this, You get shown a window letting you choose the server where to add the bot, simply select the server and click **Authorize**.
+
+```
+
+## Built With
+
+* [ATOM](https://atom.io/) - The IDE
+* [Node.js](https://nodejs.org/) - The Backend
+* [Discord.js](https://discord.js.org/#/) - The Documentation and Plugin
---
+++ bot.js
@@ -0,0 +1,147 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const music = require('discord.js-music-v11');
+var giphy = require('giphy-api')();
+
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? ' HEADS' : ' TAILS';
+}
+function randomIntInc (low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong");
+  }
+});
+
+/*client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "play")) {
+    const args = message.content.slice(config.prefix).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    let song = args[1]
+    play(song)
+  }
+});*/
+
+client.on('message', message => {
+  if (message.content.startsWith(config.prefix + "summon")) {
+    const voiceChannel = message.member.voiceChannel;
+
+    voiceChannel.join()
+    .then(connection => console.log('Connected!'))
+    .catch(console.error);
+  }
+});
+
+client.on('message', message => {
+  if (message.content.startsWith(config.prefix + "dismiss")) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.leave()
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("<@354025589851815950>")) {
+    message.channel.send(message.author + ", Unknown Command, please check !help.");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "coinflip")) {
+    message.channel.send(message.author + ", The coin landed on" + coinFlip() + ".");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "decide")) {
+    const args = message.content.slice(config.prefix).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    let first = args[0]
+    let second = args[2]
+    desc = randomIntInc(1,2)
+    if (desc===1){
+      choice=first
+    }
+    if (desc===2){
+      choice=second
+    }
+    message.channel.send("I have decided on " + choice + ".");
+  }
+});
+
+//ROCK PAPER SCISSORS
+var res=0;
+var result="";
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "rps")) {
+    res = randomIntInc(1,3);
+    //scissors 1
+    //paper 2
+    //rock 3
+    if(res===1){
+      result="scissors";
+    }else if(res===2){
+      result="paper";
+    }else if(res===3){
+      result="rock";
+    }
+    if(message.content==="!rps rock"){
+      if(res===2){
+        //win
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Win!");
+      }else if(res===1){
+        //lose
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Lost!");
+      }else if(res===3){
+        //draw
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", Draw");
+      }/*else{
+        message.channel.sendMessage("You have to /start the game")
+      }*/
+    }else if(message.content==="!rps paper"){
+      if(res===1){
+        //win
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Win!");
+      }else if(res===3){
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Lost!");
+      }else if(res===2){
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", Draw");
+      }
+    }else if(message.content==="!rps scissors"){
+      if(res===3){
+        //win
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Win!");
+      }else if(res===2){
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", I Lost!");
+      }else if(res===1){
+        message.channel.sendMessage("I got "+ result);
+        message.channel.sendMessage(message.author + ", Draw");
+      }
+    }}
+});
+
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "help")) {
+    message.channel.send("Available Commands:\n !ping : sends a pong response when online.\n !coinflip : flips a coin and returns heads or tails.\n !exbot\n !exbot\n !exbot\n");
+  }
+});
+
+music(client);
+client.login(config.token);
